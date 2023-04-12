69f8307d335925cc33736944c1d4a972724c2f2d
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-04-12 21:26:42 +0200

    finish with loading 1

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+//* html targetted ids:
+
+const containerQuote = document.getElementById("container-quote");
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter-btn");
+const nextQuoteBtn = document.getElementById("next-quote-btn");
+const loader = document.getElementById("loader");
+
 // * get quotes from api:
 let fetchedQuotesData = [];
 
 const getQuotes = async () => {
-  const URL = "https://type.fit/api/quotes";
+  loading();
+
+  const quotesURL = "https://type.fit/api/quotes";
 
   try {
-    const res = await fetch(URL);
+    const res = await fetch(quotesURL);
     fetchedQuotesData = await res.json();
     Quote();
   } catch (err) {
@@ -14,10 +25,51 @@ const getQuotes = async () => {
 };
 // * get a random quotes from the data you received:
 const Quote = () => {
-  const quote =
+  loading();
+  if (loader.hidden && containerQuote.hidden) {
+    !loader.hidden;
+    !containerQuote.hidden;
+  }
+  const quoteFromApi =
     fetchedQuotesData[Math.floor(Math.random() * fetchedQuotesData.length)];
-  console.log(quote);
+  console.log(quoteFromApi);
+  //   * DOM implementation:
+  quote.textContent = quoteFromApi.text;
+  //   * if null data:
+  if (quoteFromApi.author === null) {
+    return (author.textContent = "unknown");
+  } else {
+    author.textContent = quoteFromApi.author;
+  }
+  //   * if length of quote...:
+  if (quoteFromApi.text.length > 50) {
+    quote.classList.add("long-quote");
+  }
+
+  complete();
 };
+
+// * to show loading :
+const loading = () => {
+  loader.hidden = false;
+  containerQuote.hidden = true;
+};
+
+// * to hide loading :
+const complete = () => {
+  loader.hidden = true;
+  containerQuote.hidden = false;
+};
+
+// * tweeting:
+const tweetTheQuote = () => {
+  const tweeterURL = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`;
+  window.open(tweeterURL, "_blank");
+};
+
+// !EVENT LISTENERS:
+nextQuoteBtn.addEventListener("click", Quote);
+twitterBtn.addEventListener("click", tweetTheQuote);
 
 // * when page loads:
 getQuotes();
