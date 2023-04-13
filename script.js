adcf90dd2a00b95948e94109d94ffef01bdea8c6
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-04-13 07:56:34 +0200

    fix loading problems

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const Quote = () => {
   quote.textContent = quoteFromApi.text;
   //   * if null data:
   if (quoteFromApi.author === null) {
-    return (author.textContent = "unknown");
+    author.textContent = "unknown";
   } else {
     author.textContent = quoteFromApi.author;
   }
